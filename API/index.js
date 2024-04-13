2275f197cb3a107acbe099479119a57f19a86146
ShakishnaviM <shakishnavi27@gmail.com>
2024-04-13 10:22:38 +0530

    front end changes

--- API/index.js
+++
@@ -1,8 +0,0 @@
-// server.js
-const express = require('express');
-const app = express();
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT} !`);
-});
