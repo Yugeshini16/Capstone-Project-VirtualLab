af007557a73211b1dd79f2b7d13a31d13e6e7f22
Yugeshini Muthukkumar <myugeshini@std.appsc.sab.ac.lk>
2024-04-14 23:06:17 +0530

    Updated one icon and the footer details

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,6 +1,6 @@
 import './Footer.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faFacebook, faTwitter, faInstagram, faWhatsapp, faMailchimp, faFacebookMessenger } from '@fortawesome/free-brands-svg-icons';
 
 
 function Footer() {
@@ -23,7 +23,8 @@ function Footer() {
                         <li><FontAwesomeIcon icon={faFacebook} className=' text-3xl' /></li>
                         <li><FontAwesomeIcon icon={faTwitter}  className=' text-3xl' /></li>
                         <li><FontAwesomeIcon icon={faInstagram}  className=' text-3xl'/></li>
-                        <li><FontAwesomeIcon icon={faInstagram}  className=' text-3xl'/></li>
+                        <li><FontAwesomeIcon icon={faWhatsapp}  className=' text-3xl'/></li>
+                        <li><FontAwesomeIcon icon={faFacebookMessenger}  className=' text-3xl'/></li>
                     </ul>
                 </div>
                
@@ -31,20 +32,22 @@ function Footer() {
 
 
             <div className="fcontent">
-                <h2>Support</h2>
-                <p>Help</p>
-                <p>Review</p>
+                <h2>Quicklinks</h2>
+                <p>SignUp</p>
+                <p>SignIn</p>
+                <p>Practicals</p>
             </div>
             <div className="fcontent">
                 <h2>Support</h2>
                 <p>Help</p>
                 <p>Review</p>
+                <p>Feedback</p>
             </div>
             <div className="fcontent">
-                <h2>Support</h2>
-                <p>Help</p>
-                <p>Review</p>
-                <p>Review</p>
+                <h2>Contact Us</h2>
+                <p>+94770652537</p>
+                <p>Online Help Centre</p>
+                <p>Location</p>
             </div>
 
             
