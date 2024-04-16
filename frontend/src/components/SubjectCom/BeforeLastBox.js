ad47b8dd5dc8863359edbefd9b1c9e4d6e3038f4
Yugeshini Muthukkumar <myugeshini@std.appsc.sab.ac.lk>
2024-04-16 13:43:31 +0530

    Removed the bacgroung img in subject nav page

--- frontend/src/components/SubjectCom/BeforeLastBox.js
+++ frontend/src/components/SubjectCom/BeforeLastBox.js
@@ -7,7 +7,7 @@ function BeforeLastBox() {
             <img src="pictures/BoxPic.png" id="BoxPict" />
         </div>
         <div className="BoxPic1">
-            <h3> Sharpen your scientific skills through hands-on experiments!</h3> 
+                <h3> Sharpen your scientific skills through hands-on experiments!</h3> 
 
                 <p>Here, you'll find interactive lab  simulations, virtual experiments,</p>
                 <p>and downloadable resources to guide  you through real-world </p>
