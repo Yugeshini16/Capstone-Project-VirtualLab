d0f27e9502eea8a38e771c72896e41e3ac462d18
Yugeshini Muthukkumar <myugeshini@std.appsc.sab.ac.lk>
2024-04-18 18:44:45 +0530

    Changed a href to link by importing(Link) in SubjectNavi page

--- frontend/src/components/SubjectCom/SubjectBox.js
+++ frontend/src/components/SubjectCom/SubjectBox.js
@@ -1,14 +1,17 @@
 import './SubjectBox.css';
+import {Link} from 'react-router-dom'
+
 function SubjectBox() {
     return ( 
         <>
         <div className="SubjectBox">
-            <a href='/practical'>
+            
+            <Link to='/practical' className='link' style={{textDecoration:'none'}}>
             <div className="SubjectPic">
                 <img src="pictures/SubjectNavi_Biology.jpg" id="image-cover" />
             </div>
             <h2> Biology </h2>
-            </a>
+            </Link>
         </div>
         </>
      )
