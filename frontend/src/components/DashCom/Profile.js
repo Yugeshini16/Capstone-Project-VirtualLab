62ec0fb93dce25e2af1420715c966a6c5a9b5ee8
ShakishnaviM <shakishnavi27@gmail.com>
2024-04-19 12:26:46 +0530

    user functionality for update

--- frontend/src/components/DashCom/Profile.js
+++ frontend/src/components/DashCom/Profile.js
@@ -1,54 +1,125 @@
- import React from 'react';
- import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
+import { useRef, useState } from 'react';
 
-function Profile() {
-    const {currentUser } = useSelector((state) => state.user);
+import { useDispatch } from 'react-redux';
+import {
+  updateUserStart,
+  updateUserSuccess,
+  updateUserFailure,
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure,
+  signOut,
+} from '../../redux/user/userSlice';
 
-    return (
-       <> 
-       <div className='p-3 max-w-lg mx-auto'>
-            <h2 class=" text-3xl font-semibold text-center" >Profile</h2>
-            <form >
-                <img src={currentUser.profilePicture} alt="profile picture"
-                className='h-24 w-24 self-center cursor-pointer rounded-full object-cover'/>
+export default function Profile() {
+  const dispatch = useDispatch();
+  const [formData, setFormData] = useState({});
+  const [updateSuccess, setUpdateSuccess] = useState(false);
 
-            <input
-                defaultValue={currentUser.username}
-                type="text"
-                id="username"
-                name="password"
-                placeholder="username"
-                className='bg-slate-100 rounded-lg p-3'
-              />
+  const { currentUser, loading, error } = useSelector((state) => state.user);
+ 
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.id]: event.target.value });
+  };
 
-            <input
-                defaultValue={currentUser.email}
-                type="email"
-                id="email"
-                name="email"
-                placeholder="Email"
-                
-              />
-            <input
-                type="password"
-                id="password"
-                name="password"
-                placeholder="Password"
-                
-              />
-              <button>Update</button>  
-            </form>
-            <div className=''>
-                <span className=''>
-                    Delete Account
-                </span>
-                <span className=''>
-                     signOut
-                </span>
-            </div>
-        </div>
-        </>
-    )
-};
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      dispatch(updateUserStart());
+      const res = await fetch(`http://localhost:3001/api/user/update/${currentUser._id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(updateUserFailure(data));
+        return;
+      }
+      dispatch(updateUserSuccess(data));
+      setUpdateSuccess(true);
+    } catch (error) {
+      dispatch(updateUserFailure(error));
+    }
+  };
 
-export default Profile;
+  const handleDeleteAccount = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`http://localhost:3001/api/user/delete/${currentUser._id}`, {
+        method: 'DELETE',
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error));
+    }
+  };
+
+  const handleSignOut = async () => {
+    try {
+      await fetch('http://localhost:3001/api/auth/signout');
+      dispatch(signOut())
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
+      <img
+          src={formData.profilePicture || currentUser.profilePicture}
+          alt='profile'
+        />
+      <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+        
+        
+        <input
+          defaultValue={currentUser.username}
+          type='text'
+          id='username'
+          placeholder='Username'
+          onChange={handleChange}
+        />
+        <input
+          defaultValue={currentUser.email}
+          type='email'
+          id='email'
+          placeholder='Email'
+          onChange={handleChange}
+        />
+        <input
+          type='password'
+          id='password'
+          placeholder='Password'
+          onChange={handleChange}
+        />
+        <button >
+          {loading ? 'Loading...' : 'Update'}
+        </button>
+      </form>
+      <div className='flex justify-between mt-5'>
+        <span
+          onClick={handleDeleteAccount}
+          className='text-red-700 cursor-pointer'
+        >
+          Delete Account
+        </span>
+        <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>
+          Sign out
+        </span>
+      </div>
+      <p className='text-red-700 mt-5'>{error && 'Something went wrong!'}</p>
+      <p className='text-green-700 mt-5'>
+        {updateSuccess && 'User is updated successfully!'}
+      </p>
+    </div>
+  );
+}
